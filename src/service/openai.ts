2b06d88c13dddfd02c554a8278688d16a10880b6
haniyeh amiri <haniyehamirilol@gmail.com>
2025-06-10 12:06:19 +0330

    fix(sendMessageToOpenAi): remove timeout

--- src/service/openai.ts
+++ src/service/openai.ts
@@ -5,11 +5,7 @@ export const sendMessageToOpenAi = async (message: string) => {
     if (!message || typeof message !== "string") {
       throw new Error("message must be string");
     }
-    const res = await axios.post(
-      "http://localhost:5000/api/chat",
-      { message },
-      { timeout: 3000 }
-    );
+    const res = await axios.post("http://localhost:5000/api/chat", { message });
     return res.data.answer;
   } catch (error) {
     console.log("Error sending message", error);
