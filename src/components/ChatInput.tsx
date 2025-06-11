d92df604bfbf90de65a7aabbc834b311bcad7b86
haniyeh amiri <haniyehamirilol@gmail.com>
2025-06-11 18:22:20 +0330

    refactor(ChatInput): add form and use sendLeftIcon

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,9 +1,12 @@
-import TextareaAutosize from "react-textarea-autosize";
-import { SendIcon } from "../icons/SendIcon";
-import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
+import TextareaAutosize from "react-textarea-autosize";
+import { v4 as uuidv4 } from "uuid";
+
+import { useChat } from "../context/ChatProvider";
 import type { Message } from "./types";
-import { useChatMutation } from "../hooks/useChatMutation";
+
+import { SendIcon } from "../icons/SendIcon";
+import { SendLeftIcon } from "../icons/SendLeftIcon";
 
 type ChatInputProps = {
   setMessage: React.Dispatch<React.SetStateAction<Message[]>>;
@@ -11,7 +14,7 @@ type ChatInputProps = {
 
 export const ChatInput: React.FC<ChatInputProps> = ({ setMessage }) => {
   const [prompt, setPrompt] = useState("");
-  const { mutate } = useChatMutation();
+  const { mutate, isPending } = useChat();
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (!prompt.trim()) return;
@@ -25,7 +28,7 @@ export const ChatInput: React.FC<ChatInputProps> = ({ setMessage }) => {
     setMessage((prev) => [...prev, newUserMessage]);
 
     // send message to api and add ai response to message state
-    mutate(newUserMessage.content, {
+    mutate(prompt, {
       onSuccess: (assistantResponse: string) => {
         const assistantMessage: Message = {
           id: uuidv4(),
@@ -40,13 +43,16 @@ export const ChatInput: React.FC<ChatInputProps> = ({ setMessage }) => {
   };
 
   return (
-    <div className="flex items-center justify-center w-full fixed bottom-0  md:bottom-20 ">
-      <div
-        className={`relative bg-[#eee] w-full md:w-[55%] ${
-          prompt.length > 39 ? "pb-13 md:pb-10" : "pb-0"
+    <div className="flex items-center justify-center w-full fixed bottom-0  md:bottom-12 z-[1000]">
+      <form
+        onSubmit={handleSubmit}
+        className={`relative w-full md:w-[55%] border-top outline-white outline-10 ${
+          prompt.length > 39 ? "pb-13 md:pb-10 bg-[#eee]" : "pb-0 bg-white pt-2"
         } rounded-3xl flex justify-center items-center`}
       >
         <TextareaAutosize
+          disabled={isPending}
+          spellCheck="false"
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)}
           placeholder="Ask anything"
@@ -55,7 +61,6 @@ export const ChatInput: React.FC<ChatInputProps> = ({ setMessage }) => {
           className="w-full p-5 no-scrollbar placeholder:text-[14px] placeholder:md:text-[1rem] leading-relaxed resize-none overflow-y-auto bg-[#eee] focus:outline-none text-[#333] rounded-[1.2rem]"
         />
         <button
-          onClick={handleSubmit}
           className={`${
             prompt.length > 39 ? "bottom-3 md:bottom-2" : ""
           }  absolute right-4  cursor-pointer focus:outline-none`}
